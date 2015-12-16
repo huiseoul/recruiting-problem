e74ccb09937dea9d2685a0e6d6e8b5e6476e2829
Johnwook Choi <johnwook.choi@gmail.com>
2015-12-16 14:12:49 +0900

    Add methods for finding neighbored countries

--- lib/country_counter.rb
+++ lib/country_counter.rb
@@ -1,3 +1,4 @@
+require 'pry-byebug'
 # Solution for solving 'number of country problem'
 # see the pdf files in repository
 class CountryCounter
@@ -5,5 +6,50 @@ class CountryCounter
 
   def initialize(map = nil)
     @map = map
+  end
+
+  # return index of left-neighbored country
+  # if none, return nil
+  def left_neighbor(row = 0, col = 0)
+    return unless @map
+    return if col == 0
+    [row, col - 1] if @map[row][col - 1] == @map[row][col]
+  end
+
+  # return index of right-neighbored country
+  # if none, return nil
+  def right_neighbor(row = 0, col = 0)
+    return unless @map
+    return if col == @map[row].size
+    [row, col + 1] if @map[row][col + 1] == @map[row][col]
+  end
+
+  # return index of up-neighbored country
+  # if none, return nil
+  def up_neighbor(row = 0, col = 0)
+    return unless @map
+    return if row == 0
+    [row - 1, col] if @map[row - 1][col] == @map[row][col]
+  end
+
+  # return index of down-neighbored country
+  # if none, return nil
+  def down_neighbor(row = 0, col = 0)
+    return unless @map
+    return if row == @map.size - 1
+    [row + 1, col] if @map[row + 1][col] == @map[row][col]
+  end
+
+  # return array of indexs of neighbored_countries
+  # if none, return nil
+  def neighbors(row = 0, col = 0)
+    return [] unless @map
+    neighbored_countries = [
+      left_neighbor(row, col),
+      right_neighbor(row, col),
+      up_neighbor(row, col),
+      down_neighbor(row, col)
+    ]
+    neighbored_countries.compact
   end
 end
--- spec/country_counter_spec.rb
+++ spec/country_counter_spec.rb
@@ -12,18 +12,101 @@ describe CountryCounter do
       [4, 1, 1]
     ]
   end
+  let(:counter_without_map) { CountryCounter.new }
+  let(:counter_with_map) { CountryCounter.new(given_map) }
 
   describe '#map' do
     context 'without given map' do
-      it 'return nil' do
-        expect(CountryCounter.new.map).to be nil
+      it 'returns nil' do
+        expect(counter_without_map.map).to be nil
       end
     end
 
     context 'with given map' do
-      it 'return map' do
-        expect(CountryCounter.new(given_map).map)
+      it 'returns map' do
+        expect(counter_with_map.map)
           .to match_array(given_map)
+      end
+    end
+  end
+
+  describe '#left_neighbor' do
+    context 'without given map' do
+      it 'returns nil' do
+        expect(counter_without_map.left_neighbor).to be nil
+      end
+    end
+
+    context 'with given map' do
+      it 'returns nil, when cell without left neighbor is given' do
+        expect(counter_with_map.left_neighbor(0, 0)).to be nil
+      end
+
+      it 'returns index of neighbor, when cell with left neighbor is given' do
+        expect(counter_with_map.left_neighbor(6, 2))
+          .to match_array([6, 1])
+      end
+    end
+  end
+
+  describe '#right_neighbor' do
+    # Skips for without given map context
+    it 'returns nil, when cell without right neighbor is given' do
+      expect(counter_with_map.right_neighbor(0, 2)).to be nil
+    end
+
+    it 'returns index of neighbor, when cell with right neighbor is given' do
+      expect(counter_with_map.right_neighbor(6, 1))
+        .to match_array([6, 2])
+    end
+  end
+
+  describe '#up_neighbor' do
+    # Skips for without given map context
+    it 'returns nil, when cell without up neighbor is given' do
+      expect(counter_with_map.up_neighbor(0, 1)).to be nil
+    end
+
+    it 'returns index of neighbor, when cell with up neighbor is given' do
+      expect(counter_with_map.up_neighbor(3, 1))
+        .to match_array([2, 1])
+    end
+  end
+
+  describe '#down_neighbor' do
+    # Skips for without given map context
+    it 'returns nil, when cell without down neighbor is given' do
+      expect(counter_with_map.down_neighbor(6, 0)).to be nil
+    end
+
+    it 'returns index of neighbor, when cell with down neighbor is given' do
+      expect(counter_with_map.down_neighbor(0, 2))
+        .to match_array([1, 2])
+    end
+  end
+
+  describe '#neighbored_countries' do
+    context 'without given map' do
+      it 'returns empty array' do
+        expect(counter_without_map.neighbors)
+          .to match_array([])
+      end
+    end
+
+    context 'with given map' do
+      it 'returns neighbored_countries for given cell, case 1' do
+        expect(counter_with_map.neighbors(0, 0))
+          .to match_array([])
+      end
+
+      it 'returns neighbored_countries for given cell, case 1' do
+        expect(counter_with_map.neighbors(5, 2))
+          .to match_array([[5, 1], [4, 2]])
+      end
+
+      it 'returns neighbored_countries for given cell, case 1' do
+        expect(counter_with_map.neighbors(3, 1))
+          .to match_array([[3, 0], [3, 2], [2, 1]])
       end
     end
   end
