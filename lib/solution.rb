a19ab316da14e6b1289af6a4956e9cee58fabf45
Johnwook Choi <johnwook.choi@gmail.com>
2015-12-16 11:20:31 +0900

    Solution initialization
    Solution has a problem to solve

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,2 +1,9 @@
+# Solution for solving 'number of country problem'
+# see the pdf files in repository
 class Solution
+  attr_reader :problem
+
+  def initialize(problem = nil)
+    @problem = problem
+  end
 end
